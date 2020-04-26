480a183085329937e14fc050062e99180d7da065
BERNARD Damien <damien.bernard@epitech.eu>
2020-04-26 17:07:14 +0200

    update(cat): Moving cat outside Registercat function.

--- cmd/codebase/cat.go
+++ cmd/codebase/cat.go
@@ -5,16 +5,20 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func Blalal(args []string) {
+	repo := []string{"main.go"}
+	found := make([]bool, len(args))
+	parser := parsingRepo{nil, contentFound{}, args, found}
+	ParseRepo(&parser, repo)
+}
+
 func registerCat(parentCmd *cobra.Command) {
 	var catCmd = &cobra.Command{
 		Use:   "cat elem...",
 		Short: "Prints the requested elements of the codebase.",
 		Run: func(_ *cobra.Command, args []string) {
 			fmt.Println("Printing: ", args)
-			repo := []string{"main.go"}
-			found := make([]bool, len(args))
-			parser := parsingRepo{nil, contentFound{}, args, found}
-			ParseRepo(&parser, repo)
+			Blalal(args)
 		},
 	}
 
