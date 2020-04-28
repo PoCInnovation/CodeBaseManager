529244913f319c6d40f6db7997094ea3e9264083
BERNARD Damien <damien.bernard@epitech.eu>
2020-04-28 13:17:07 +0200

    add(cat): Adding function to find go function.

--- cmd/codebase/cat.go
+++ cmd/codebase/cat.go
@@ -2,7 +2,10 @@ package codebase
 
 import (
 	"fmt"
+	"github.com/PoCFrance/CodeBaseManager/modules/codebase"
 	"github.com/spf13/cobra"
+	"log"
+	"strings"
 )
 
 func registerCat(parentCmd *cobra.Command) {
@@ -22,15 +25,70 @@ func registerCat(parentCmd *cobra.Command) {
 
 func cat(args []string) {
 	// TODO: Change repo parsing and evaluate repo language
-	repo := []string{"."}
+	repo := []string{"cmd"}
 	parser := parsingRepo{
-		args:    args,
-		content: contentFound{},
-		parser:  CatParser,
-		fileFct: CatFile,
+		args:       args,
+		content:    contentFound{},
+		parser:     CatParser,
+		manageFile: CatFile,
 	}
 	for _, module := range repo {
 		RepoParser(module, parser)
 	}
 	PrintResult(args, parser)
+}
+
+func CatParser(name string, control parsingRepo) {
+	for _, arg := range control.args {
+		splitName := strings.Split(name, "/")
+		splitLen := len(splitName)
+		if splitLen == 0 {
+			log.Printf("Cannot Split %s\n", name)
+		}
+
+		if arg == splitName[splitLen-1] {
+			// TODO: refacto parsing to use fctPtr -> common ground for cat and find
+			//var err error
+			control.content[arg], _ = control.manageFile(control.content[arg], name)
+		} else {
+			fmt.Println(name)
+			control.content[arg], _ = CatFunction(control.content[arg], name, arg)
+		}
+
+	}
+}
+
+func CatFile(controlContent map[string]string, name string) (map[string]string, error) {
+	content, err := codebase.GetFile(name)
+	if err != nil {
+		return controlContent, err
+	}
+
+	if controlContent != nil {
+		controlContent[name] = *content
+	} else {
+		controlContent = map[string]string{}
+		controlContent[name] = *content
+	}
+
+	return controlContent, nil
+}
+
+func CatFunction(controlContent map[string]string, name, arg string) (map[string]string, error) {
+	content, err := codebase.GetFile(name)
+	if err != nil {
+		return controlContent, err
+	}
+
+	if found := catGoFunction(*content, arg); found != nil {
+		//read content to find function
+		//fmt.Println(*found)
+		if controlContent != nil {
+			controlContent[name] = *found
+		} else {
+			controlContent = map[string]string{}
+			controlContent[name] = *found
+		}
+	}
+	return controlContent, nil
 }
--- cmd/codebase/find.go
+++ cmd/codebase/find.go
@@ -3,6 +3,8 @@ package codebase
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"log"
+	"strings"
 )
 
 func registerFind(parentCmd *cobra.Command) {
@@ -25,13 +27,40 @@ func find(args []string) {
 	// TODO: Change repo parsing and evaluate repo language
 	repo := []string{"."}
 	parser := parsingRepo{
-		args:    args,
-		content: contentFound{},
-		parser:  FindParser,
-		fileFct: FindFile,
+		args:       args,
+		content:    contentFound{},
+		parser:     FindParser,
+		manageFile: FindFile,
 	}
 	for _, module := range repo {
 		RepoParser(module, parser)
 	}
 	PrintResult(args, parser)
+}
+
+func FindParser(name string, control parsingRepo) {
+	for _, arg := range control.args {
+		splitName := strings.Split(name, "/")
+		splitLen := len(splitName)
+		if splitLen == 0 {
+			log.Printf("Cannot Split %s\n", name)
+		}
+
+		if arg == splitName[splitLen-1] {
+			// TODO: refacto parsing to use fctPtr -> common ground for cat and find
+			control.content[arg], _ = control.manageFile(control.content[arg], name)
+		} else {
+			//read content to find function
+		}
+	}
+}
+
+func FindFile(controlContent map[string]string, name string) (map[string]string, error) {
+	if controlContent != nil {
+		controlContent[name] = name
+	} else {
+		controlContent = map[string]string{}
+		controlContent[name] = name
+	}
+	return controlContent, nil
 }
