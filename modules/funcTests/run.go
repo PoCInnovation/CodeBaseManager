40ad829081a2f59f5fc06701469a3432082a9be1
Quentin Veyrenc <quentin.veyrenc@epitech.eu>
2020-05-02 10:51:51 +0200

    refactor(module-ft): simplified run by moving most of the code to their respective target (types)

--- modules/funcTests/run.go
+++ modules/funcTests/run.go
@@ -12,25 +12,11 @@ func Run(_ []string) {
 		return
 	}
 	// TODO: if no bin ask build module for binary
+	// TODO: use exec.lookPath to make sure our bin's ok
 	for _, test := range cfg.Tests {
-		fmt.Printf("Setting up %s...\n", test.Name)
-		test.Opt.SetCommon(&cfg.Common.Opt)
-		test.myExec.Set(cfg.Common.Bin, test.Args...)
-		if cfg.Common.RefBin != "" {
-			test.refExec.Set(cfg.Common.RefBin, test.RefArgs...)
-		}
-		// TODO: Setup tests
-		//       - Commands
-		//          - their envs
-		//          - pipes & stdin
-		test.myExec.Run()
-		fmt.Println(test.myExec.outBuf.String(), test.myExec.errBuf.String())
-		// TODO: Run tests
-		//       - run pre
-		//       - run /!\ Options /!\
-		//       - run post
-		// TODO: compare with expected.
-		//
+		test.Init(&cfg.Common)
+		test.Run()
+		// TODO: Analyse results
 	}
 	// TODO: show results
 }
