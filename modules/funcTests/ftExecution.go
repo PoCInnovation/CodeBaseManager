a1d7ecfb2c4dc0c50c3f436bd16b016afe18b96b
Quentin Veyrenc <quentin.veyrenc@epitech.eu>
2020-05-02 10:49:48 +0200

    feat(module-ft): added command setup (retrieve their output & set their env) & execution

--- modules/funcTests/ftExecution.go
+++ modules/funcTests/ftExecution.go
@@ -3,6 +3,7 @@ package funcTests
 import (
     "bytes"
     "fmt"
+    "os"
     "os/exec"
 )
 
@@ -12,14 +13,32 @@ type ftExecution struct {
     errBuf bytes.Buffer
 }
 
-func (e *ftExecution) Set(bin string, args ...string) {
+func (e *ftExecution) Set(inter *ftInteractions, bin string, args ...string) {
     e.cmd = exec.Command(bin, args...)
+
     e.cmd.Stdout = &e.outBuf
     e.cmd.Stderr = &e.errBuf
+
+    if inter.Env != nil {
+        e.cmd.Env = inter.Env
+    } else {
+        e.cmd.Env = os.Environ()
+    }
+    e.cmd.Env = append(e.cmd.Env, inter.AddEnv...)
+
+    // TODO: - stdin
+    //       - pipes
+
 }
 
 func (e *ftExecution) Run() {
     if err := e.cmd.Run(); err != nil {
         fmt.Println(err)
     }
+}
+
+func QuickRun(cmdLine string) error {
+    cmd := exec.Command("bash", "-c", cmdLine)
+    _, err := cmd.CombinedOutput()
+    return err
 }
