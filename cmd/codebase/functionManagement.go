365d4c76be2f7312dfbba49b039bc9b380ba69d6
BERNARD Damien <damien.bernard@epitech.eu>
2020-05-04 17:02:15 +0200

    update(cat/find): Adding Python function cat/find and files for testing.

--- cmd/codebase/functionManagement.go
+++ cmd/codebase/functionManagement.go
@@ -7,6 +7,12 @@ import (
 )
 
 //TODO: transform fct into 2 fields of catFct and findFct
+const (
+	FIND = 0
+	CAT  = 1
+)
+
+//TODO: Manage several functions/methods in same file
 type findFctArray struct {
 	language   string
 	extensions []string
@@ -18,13 +24,20 @@ type findFctArray struct {
 //var pythonExtension = []string{".py", ".pyc"}
 
 var catTargetFcts = []findFctArray{
-	{language: "C", fct: catCFunction,
+	{
+		language: "C", fct: catCFunction,
 		extensions: []string{".c", ".h"},
 		regex:      "((?m)^(\\w+(\\s+)?){1,3})%s((\\((.*?)\\))(\\s*)\\{(\\s*?.*?)*?\n\\})",
 	},
-	{language: "Go", fct: catGoFunction,
+	{
+		language: "Go", fct: catGoFunction,
 		extensions: []string{".go"},
 		regex:      "(?m)^((\\t| )*?)func %s\\((.+)\\{(\\s*?.*?)*?\n\\}\n",
+	},
+	{
+		language: "Python", fct: catPythonFunction,
+		extensions: []string{".py"},
+		regex:      "(((?:^[ \t]*)*@(.*)\n)(?:^[ \t]*)def %s\\(.*\\):(\\s)*((?=.*?[^ \t\n]).*\r?\n?)*\n)",
 	},
 }
 
@@ -36,6 +49,11 @@ var findTargetFcts = []findFctArray{
 	{language: "Go", fct: findGoFunction,
 		extensions: []string{".go"},
 		regex:      "(?m)^((\\t| )*?)func %s\\((.+)\\{(\\s*?.*?)*?\n\\}\n",
+	},
+	{
+		language: "Python", fct: findPythonFunction,
+		extensions: []string{".py"},
+		regex:      "(((?:^[ \t]*)*@(.*)\n)(?:^[ \t]*)def %s\\(.*\\):(\\s)*((?=.*?[^ \t\n]).*\r?\n?)*\n)",
 	},
 }
 
@@ -65,12 +83,20 @@ func findTargetFunction(reg, fileContent, fctName string) *string {
 }
 
 func catGoFunction(fileContent, fctName string) *string {
-	reg := "(?m)^((\\t| )*?)func " + fctName + "\\((.+)\\{(\\s*?.*?)*?\n\\}\n"
+	reg := fmt.Sprintf("(?m)^((\\t| )*?)func %s\\((.+)\\{(\\s*?.*?)*?\n\\}\n", fctName)
 	return catTargetFunction(reg, fileContent)
 }
 
 func catCFunction(fileContent, fctName string) *string {
-	reg := "((?m)^(\\w+(\\s+)?){1,3})" + fctName + "((\\((.*?)\\))(\\s*)\\{(\\s*?.*?)*?\n\\})"
+	reg := fmt.Sprintf("((?m)^(\\w+(\\s+)?){1,3})%s((\\((.*?)\\))(\\s*)\\{(\\s*?.*?)*?\n\\})", fctName)
+	return catTargetFunction(reg, fileContent)
+}
+
+func catPythonFunction(fileContent, fctName string) *string {
+	//fmt.Println(fileContent, "\n" + fctName)
+	reg := fmt.Sprintf("((((?m)^[ \t]*)@(.*)\n)*((?m)^[ \t]*)def %s\\(.*\\):(\\s)*((.*?[^ \t\n]).*\r?\n?)*)", fctName)
+	// TODO: DOING FUCKING CLASS.
+	//reg := fmt.Sprintf("((?m)^[ \t]*)class %s(\\(([^\r\n\f\v])*\\))*:\n((.*\\s*?)*)\n*", fctName)
 	return catTargetFunction(reg, fileContent)
 }
 
@@ -80,6 +106,14 @@ func findGoFunction(fileContent, fctName string) *string {
 }
 
 func findCFunction(fileContent, fctName string) *string {
-	reg := "((?m)^(\\w+(\\s+)?){1,3})" + fctName + "((\\((.*?)\\))(\\s*)\\{(\\s*?.*?)*?\n\\})"
+	reg := fmt.Sprintf("(?m)^((\\t| )*?)func %s\\((.+)\\{(\\s*?.*?)*?\n\\}\n", fctName)
+	return findTargetFunction(reg, fileContent, fctName)
+}
+
+func findPythonFunction(fileContent, fctName string) *string {
+	//fmt.Println(fileContent, "\n" + fctName)
+	reg := fmt.Sprintf("((((?m)^[ \t]*)@(.*)\n)*((?m)^[ \t]*)def %s\\(.*\\):(\\s)*((.*?[^ \t\n]).*\r?\n?)*)", fctName)
+	// TODO: DOING FUCKING CLASS.
+	//reg := fmt.Sprintf("((?m)^[ \t]*)class %s(\\(([^\r\n\f\v])*\\))*:\n((.*\\s*?)*)\n*", fctName)
 	return findTargetFunction(reg, fileContent, fctName)
 }
