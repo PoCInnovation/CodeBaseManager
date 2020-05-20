c57e57cdc30678020bf05baad47f998b2e0819e9
Quentin Veyrenc <quentin.veyrenc@epitech.eu>
2020-05-20 20:27:11 +0200

    feat: added args handling in ft run

--- modules/funcTests/run.go
+++ modules/funcTests/run.go
@@ -4,20 +4,22 @@ import (
 	"fmt"
 )
 
-func Run(_ []string) {
+func Run(av []string) {
 	// TODO: More flexibility on path
-	cfg, err := NewConfigFT(".cbm/template/cantFail.toml")
-	if err != nil {
-		fmt.Println(err)
-		// TODO: Would you like to continue ? yes | exit
-		return
+	for _, fp := range av {
+		cfg, err := NewConfigFT(fp)
+		if err != nil {
+			fmt.Println(err)
+			// TODO: Would you like to continue ? yes | exit
+			return
+		}
+		// TODO: if no bin ask build module for binary
+		// TODO: use exec.lookPath to make sure our bin's ok
+		for _, test := range cfg.Tests {
+			test.Init(&cfg.Common)
+			test.Run()
+			test.GetResults()
+		}
+		// TODO: show results
 	}
-	// TODO: if no bin ask build module for binary
-	// TODO: use exec.lookPath to make sure our bin's ok
-	for _, test := range cfg.Tests {
-		test.Init(&cfg.Common)
-		test.Run()
-		test.GetResults()
-	}
-	// TODO: show results
 }
