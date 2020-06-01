ef3fdcd7e1cae664dbbb56cc0ddb0a4183c6a58f
Quentin Veyrenc <quentin.veyrenc@epitech.eu>
2020-06-01 13:31:54 +0200

    fix(repl/codebase): now uses cbm's cat/find

--- cmd/codebase/codebase.go
+++ cmd/codebase/codebase.go
@@ -14,7 +14,8 @@ func RegisterCmd(parentCmd *cobra.Command) {
 		Run: func(cmd *cobra.Command, _ []string) {
 			sh := REPL.NewShell("CodeBase")
 			accepted := REPL.Builtins{
-				//TODO: add builtins
+				"cat": cat,
+				"find": find,
 			}
 			sh.Run(accepted)
 		},
