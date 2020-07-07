6d8100f33c24402c5d8fa54bed32fb923c430010
Quentin Veyrenc <quentin.veyrenc@epitech.eu>
2020-07-08 01:21:39 +0200

    feat(module/server): added basic cli func (add/drop/list repo's), they're currently behavior less

---
+++ cmd/server/server.go
@@ -0,0 +1,16 @@
+package server
+
+import "github.com/spf13/cobra"
+
+func RegisterCmd(parentCmd *cobra.Command) {
+    var serverCmd = &cobra.Command{
+        Use: "server",
+        Short: "Allows you to interact with CBM's server",
+    }
+
+    serverCmd.Args = cobra.ExactArgs(0)
+    registerAdd(serverCmd)
+    registerDrop(serverCmd)
+    registerList(serverCmd)
+    parentCmd.AddCommand(serverCmd)
+}
