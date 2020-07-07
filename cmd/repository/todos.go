8414ae6d63074a38e96240f9df1808e681de17c6
Quentin Veyrenc <quentin.veyrenc@epitech.eu>
2020-07-08 00:26:53 +0200

    update(todos): added context option to display the surrounding of the TODO, improved basic print & todo detection

--- cmd/repository/todos.go
+++ cmd/repository/todos.go
@@ -6,14 +6,17 @@ import (
 )
 
 func registerTodos(parentCmd *cobra.Command) {
+    var ctxDepth int
     var todosCmd = &cobra.Command{
         Use: "todos",
         Short: "Displays all TODO in your repository",
         Run: func(_ *cobra.Command, _ []string) {
-            repository.DisplayTodos()
+            repository.DisplayTodos(ctxDepth)
         },
     }
 
     todosCmd.Args = cobra.ExactArgs(0)
+    todosCmd.Flags().IntVarP(&ctxDepth, "context-depth", "c", 0,
+        "Number of lines above & below todo to be shown")
     parentCmd.AddCommand(todosCmd)
 }
