505320cfcfbbcab46592230d75c8b2262d8d4372
BERNARD Damien <damien.bernard@epitech.eu>
2020-08-02 14:53:00 +0200

    Update(backend): Adding todo's model methods.

--- backend/models/Todo.go
+++ backend/models/Todo.go
@@ -7,3 +7,24 @@ type Todo struct {
 	Name string `gorm:"size:255;not null" json:"name"`
 	Path string `gorm:"size:255;not null" json:"path"`
 }
+
+func (t *Todo) Save(db *gorm.DB) (*Todo, error) {
+	if err := db.Create(&t).Error; err != nil {
+		return &Todo{}, err
+	}
+	return t, nil
+}
+
+func (t *Todo) Update(db *gorm.DB) (*Todo, error) {
+	if err := db.Update(&t).Error; err != nil {
+		return &Todo{}, err
+	}
+	return t, nil
+}
+
+func (t *Todo) Delete(db *gorm.DB) (*Todo, error) {
+	if err := db.Delete(&t).Error; err != nil {
+		return &Todo{}, err
+	}
+	return t, nil
+}
