09bf1a5202f366b8f7ded8cda4f5f75f69be6743
BERNARD Damien <damien.bernard@epitech.eu>
2020-08-14 18:27:57 +0200

    Update(Install): Formatting install scripts file.

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 
 echo -e "\e[1;94mBuilding CBM...\e[0m"
 go build -o cbm main.go
-if [[ $? != 0 ]]; then echo "CodeBaseManager Couldn't build"; exit 1; fi
+if [[ $? != 0 ]]; then
+  echo "CodeBaseManager Couldn't build"
+  exit 1
+fi
 echo -e "\e[1;94mMoving CBM to /usr/bin\e[0m"
 sudo mv cbm /usr/bin/
 
@@ -20,14 +23,16 @@ function validateHome() {
     echo -e "\e[1;94mPlease validate your HOME directory: $HOME (Yy/Nn)\e[0m"
     read -r yn
     case $yn in
-        [Yy]* ) USER_HOME="$HOME";
-          CBM_GLOBAL_PATH="$USER_HOME/$CBM_DIRNAME"
-          mkdir -p "$USER_HOME/$CBM_DIRNAME";
-          break;;
-        [Nn]* ) exit;;
-        * ) echo -e "\e[1;94mPlease answer yes or no.\e[0m";;
+    [Yy]*)
+      USER_HOME="$HOME"
+      CBM_GLOBAL_PATH="$USER_HOME/$CBM_DIRNAME"
+      mkdir -p "$USER_HOME/$CBM_DIRNAME"
+      break
+      ;;
+    [Nn]*) exit ;;
+    *) echo -e "\e[1;94mPlease answer yes or no.\e[0m" ;;
     esac
-done
+  done
 }
 
 function installBackend() {
@@ -36,7 +41,7 @@ function installBackend() {
 
   rm -rfd "$CBM_GLOBAL_BACKEND_DIR"
   cp -r "$CBM_BACKEND_DIR" "$CBM_GLOBAL_BACKEND_DIR"
-  echo "PORT=$CBM_PORT" > "$CBM_API_ENV_PATH"
+  echo "PORT=$CBM_PORT" >"$CBM_API_ENV_PATH"
 
   cd "$CBM_GLOBAL_BACKEND_DIR" && make api-clean-stop && make api-start
 }
@@ -44,14 +49,17 @@ function installBackend() {
 function cbmPort() {
 
   while true; do
-    if [[ $CBM_PORT != "" ]]; then echo "\n$CBM_PORT didn't work" ;fi
+
+    if [[ $CBM_PORT != "" ]]; then echo -e "\n\e[1;94m$CBM_PORT didn't work\e[0m"; fi
+
     echo -e "\e[1;94mPlease enter a valid port on which Cbm Backend could run:\e[0m"
     read -r CBM_PORT
     echo -e "\e[1;94mLaunching CodeBaseManager Backend ...\n\n\e[0m"
 
-    if ! installBackend ;then continue ; else break ; fi
-done
-echo -e "\e[1;94mApi Running ... \n\e[0m"
+    if ! installBackend; then continue; else break; fi
+
+  done
+  echo -e "\e[1;94mApi Running ... \n\e[0m"
 }
 
 validateHome
