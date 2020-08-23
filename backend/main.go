3d3a24b27a6712dcca56d0e11db5c27b709ab5a0
Quentin Veyrenc <quentin.veyrenc@epitech.eu>
2020-08-23 20:51:38 +0200

    update(backend): re-ordered startup action (server -> watcher instead of watcher -> server)

--- backend/main.go
+++ backend/main.go
@@ -7,19 +7,19 @@ import (
 )
 
 func main() {
-	//Setup the watcher to keep track of projects' files & gets ready to properly close it
-	stopWatcher := make(chan struct{})
-	go watcher.Run(stopWatcher)
-	defer close(stopWatcher)
-
 	// Setup the server for CLI's needs & gets ready to properly close it
 	server, stopServer := NewServer()
 	routes.ApplyRoutes(server.Router)
 	defer stopServer()
 
+	//Setup the watcher to keep track of projects' files & gets ready to properly close it
+	stopWatcher := make(chan struct{})
+	go watcher.Run(stopWatcher)
+	defer close(stopWatcher)
+
 	// Starts the server
 	log.Println("Server runs on http://localhost:" + server.Port)
 	if err := server.Router.Run(); err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
 }
