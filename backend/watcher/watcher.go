254e5c1d526a4dde7300d7f71ea6e23bcc1c5d4d
Quentin Veyrenc <quentin.veyrenc@epitech.eu>
2020-08-23 20:53:02 +0200

    update(watcher): added complete initialization of the watcher (needs testing)

--- backend/watcher/watcher.go
+++ backend/watcher/watcher.go
@@ -1,34 +1,117 @@
 package watcher
 
 import (
+	"cbm-api/controllers"
+	"cbm-api/model"
+	"fmt"
+	"github.com/BurntSushi/toml"
 	"github.com/rjeczalik/notify"
 	"log"
+	"os"
+	"path/filepath"
 	"time"
 )
 
 // Documentation on notify: https://godoc.org/github.com/rjeczalik/notify#example-Watch--LinuxMove
 
-const watchedEvents = notify.InCreate | notify.InDelete | notify.InDeleteSelf | notify.InModify | notify.InMovedFrom | notify.InMovedTo | notify.InMoveSelf
+type config struct {
+	Repos []string `toml:"repositories"`
+	Files []string `toml:"files"`
+	Events []string `toml:"events"`
+	Ignored []string `toml:"ignore"`
+}
 
-func start() chan notify.EventInfo {
-	log.Println("Initializing watcher")
-	// TODO: Load config file?
-	// TODO: Get repository list (based on config?)
-	watcher := make(chan notify.EventInfo, 2)
-	// TODO: Set watchpoint for everything (except .git, ...) (or based on config?)
-	return watcher
+var TellWatcher chan string
+
+const (
+	watchedEvents = notify.InCreate | notify.InDelete | notify.InDeleteSelf | notify.InModify | notify.InMovedFrom | notify.InMovedTo | notify.InMoveSelf
+	configFile = "watcher.toml"
+	configGlobal = "/" + configFile
+	configLocal = ".cbm/" + configFile
+	)
+
+func loadConfig() (*config, error) {
+	cfg := &config{}
+	md, err := toml.DecodeFile(configGlobal, cfg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load config: %v", err)
+	}
+	ignored := md.Undecoded()
+	if len(ignored) != 0 {
+		return nil, fmt.Errorf("watcher's config was ignored:\n%v", ignored)
+	}
+	return cfg, nil
+}
+
+func isIgnored(name string, in []string) bool {
+	for _, v := range in {
+		if v == name {
+			return true
+		}
+	}
+	return false
+}
+
+func isBanned(name string) bool {
+	return name == ".git" || name == ".vscode" || name == ".idea"
+}
+
+func loadProjects(projects []model.Project, cfg *config, c chan notify.EventInfo) {
+	for _, proj := range projects {
+
+		log.Println("Loading " + proj.Name)
+		if isIgnored(proj.Name, cfg.Ignored) || isBanned(proj.Name){
+			continue
+		}
+
+		if err := filepath.Walk(proj.Path, func(name string, info os.FileInfo, err error) error {
+			log.Println("Im here " + name)
+			if isIgnored(name, cfg.Ignored) || isBanned(name) {
+				return fmt.Errorf("%s: ignored\n%v", name, err)
+			}
+			if tmpErr := notify.Watch(name, c, watchedEvents); tmpErr != nil {
+				return fmt.Errorf("failed to add watchpoint for '%s': %v\n%v", name, tmpErr, err)
+			}
+			return err
+		}); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func start() (chan notify.EventInfo, error) {
+	log.Println("Initializing Watcher")
+
+	cfg, err := loadConfig()
+	if err != nil {
+		return nil, fmt.Errorf("Watcher failed to start: %v", err)
+	}
+
+	projects, err := controllers.ListProjects()
+	ch := make(chan notify.EventInfo, 2)
+	if len(projects) == 0 {
+		return ch, nil
+	} else if err != nil && len(projects) != 0 {
+		return nil, fmt.Errorf("I ate my life: %v", err)
+	}
+
+	loadProjects(projects, cfg, ch)
+	return ch, nil
 }
 
 func Run(stop chan struct{}) {
-	watcher := start()
-	defer notify.Stop(watcher)
+	ch, err := start()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer notify.Stop(ch)
 	for {
 		select {
 		case <-stop:
 			log.Println("Watcher's shutting down")
 			return
 		default:
-			for events := range watcher {
+			for events := range ch {
 				log.Println("received:", events)
 			}
 			time.Sleep(5 * time.Second)
