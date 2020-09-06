dbd73c41ce4833bab57a54eeea081acb11bbe05e
Quentin Veyrenc <quentin.veyrenc@epitech.eu>
2020-09-06 22:27:02 +0200

    remove(backend): watcher is now it's own program

--- backend/main.go
+++ backend/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"cbm-api/routes"
-	"cbm-api/watcher"
 	"log"
 )
 
@@ -11,11 +10,6 @@ func main() {
 	server, stopServer := NewServer()
 	routes.ApplyRoutes(server.Router)
 	defer stopServer()
-
-	//Setup the watcher to keep track of projects' files & gets ready to properly close it
-	stopWatcher := make(chan struct{})
-	go watcher.Run(stopWatcher)
-	defer close(stopWatcher)
 
 	// Starts the server
 	log.Println("Server runs on http://localhost:" + server.Port)
